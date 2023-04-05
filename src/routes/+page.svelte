d2cc72ff07386bb81806fb2f5406a589d465a874
Andreas Bresser <self@andreasbresser.de>
2023-04-05 11:53:55 +0200

    downgrade three to add shadows, add Grid and Stage for lighting and grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { Canvas, OrbitControls, T } from '@threlte/core'
   import Urdf from '../components/Urdf.svelte';
+  import Stage from '../components/Stage.svelte';
+  import Grid from '../components/Grid.svelte';
 
 </script>
 
@@ -9,8 +11,8 @@
 		rendererParameters={{logarithmicDepthBuffer: true}}>
 
 		<T.PointLight color="white" intensity={.5} position={[0, 5, 0]} />
-		<!-- T.PointLight color="blue" intensity={0.5} position={[5, 5, 0]} />
-		<T.PointLight color="yellow" intensity={0.5} position={[-5, -5, 0]} /-->
+		<T.PointLight color="blue" intensity={0.5} position={[5, 5, 0]} />
+		<T.PointLight color="yellow" intensity={0.5} position={[-5, -5, 0]} />
 
 		<T.PerspectiveCamera makeDefault let:ref={camera}
 			position={[.6, .6, .6]} fov={25}
@@ -18,6 +20,9 @@
 			 >
       <OrbitControls enableZoom={true} />
 		</T.PerspectiveCamera>
+
+		<Stage />
+		<Grid />
 		<Urdf filename="/turtlebot3_description/turtlebot3_burger.xml" />
 
 	</Canvas>
