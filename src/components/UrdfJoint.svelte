01ba696307322eef419c88206e73e7081963b606
Andreas Bresser <self@andreasbresser.de>
2024-03-29 20:10:32 +0100

    fix compile errors

--- src/components/UrdfJoint.svelte
+++ src/components/UrdfJoint.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
-  import AutoAdjust from '@smui/top-app-bar/src/AutoAdjust.svelte';
-
-	import { T } from '@threlte/core'
-  import type { IUrdfJoint } from '../models/IUrdfJoint';
-  import UrdfVisual from './UrdfVisual.svelte';
-  import { TransformControls } from '@threlte/extras';
-  import robot_urdf from '../store/robot_urdf';
-  import selection from '../store/selection';
-  import transform_tool from '../store/transform_tool';
-  import { getChildJoints } from '../UrdfParser';
-
+  import { T } from "@threlte/core";
+  import type { IUrdfJoint } from "../models/IUrdfJoint";
+  import UrdfVisual from "./UrdfVisual.svelte";
+  import { TransformControls } from "@threlte/extras";
+  import robot_urdf from "../store/robot_urdf";
+  import selection from "../store/selection";
+  import transform_tool from "../store/transform_tool";
+  import { getChildJoints } from "../UrdfParser";
 
   export let joint: IUrdfJoint;
 </script>
@@ -19,11 +16,11 @@
   <T.Group rotation={joint.origin_rpy} position={joint.origin_xyz}>
     {#each joint.child.visual as visual}
       {#if $selection == joint.child}
-      <TransformControls mode={$transform_tool}>
-        <UrdfVisual visual={visual} link={joint.child} />
-      </TransformControls>
+        <TransformControls mode={$transform_tool}>
+          <UrdfVisual {visual} link={joint.child} />
+        </TransformControls>
       {:else}
-      <UrdfVisual visual={visual} link={joint.child} />
+        <UrdfVisual {visual} link={joint.child} />
       {/if}
       {#each getChildJoints($robot_urdf, joint.child) as child}
         <svelte:self joint={child} />
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import seqPreprocessor from 'svelte-sequential-preprocessor'
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 
 /** @type {import('@sveltejs/kit').Config} */
