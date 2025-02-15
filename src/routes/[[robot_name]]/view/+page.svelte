820ba81e39883c7667ca6173cae0fc9bf145e8c7
Andreas Bresser <self@andreasbresser.de>
2025-02-15 14:10:36 +0100

    view files again

--- src/routes/[[robot_name]]/view/+page.svelte
+++ src/routes/[[robot_name]]/view/+page.svelte
@@ -1,25 +1,20 @@
 <!-- only view the robot, do not edit it -->
 <script lang="ts">
-  import { page } from '$app/stores';
-
+  import { page } from '$app/state';
   import { onMount } from 'svelte';
 
   import { Canvas, T } from '@threlte/core';
   import { OrbitControls } from '@threlte/extras';
-  import UrdfThree from '$lib/components/UrdfThree.svelte';
-  import ThreeStage from '$lib/components/ThreeStage.svelte';
 
-  import { UrdfParser } from '$lib/UrdfParser';
-
-  import robot_urdf from '$lib/store/robot_urdf';
   import { AppContent } from '@smui/drawer';
-  import Grid from '$lib/components/Grid.svelte';
+  import { Grid, ThreeStage, urdf_viewer_state, UrdfParser, UrdfThree } from 'urdf-viewer';
+  import { WebGLRenderer } from 'three';
 
   let innerHeight = 0;
   let innerWidth = 0;
 
-  let prefix = $page.url.href + '/../..';
-  const robot_name = $page.params.robot_name;
+  let prefix = page.url.href + '/../..';
+  const robot_name = page.params.robot_name;
 
   const filename = `turtlebot3_description/${robot_name}.xml`;
   const parser = new UrdfParser(`${prefix}/${filename}`, prefix);
@@ -27,33 +22,40 @@
   onMount(async () => {
     let promise = parser.load();
     let code = await promise;
-    robot_urdf.set(parser.fromString(code));
+    urdf_viewer_state.robot = parser.fromString(code);
   });
 </script>
 <svelte:window
-  bind:innerHeight={innerHeight}
-  bind:innerWidth={innerWidth} />
+  bind:innerHeight
+  bind:innerWidth />
+<main class="main-content">
+  <div style:width={innerWidth - 250 + 'px'} style:height={innerHeight + 'px'}>
+    <Canvas
+    createRenderer={(canvas) => {
+      return new WebGLRenderer({
+        canvas,
+        alpha: true,
+        powerPreference: 'high-performance',
+        logarithmicDepthBuffer: true
+      })}}
+      shadows>
 
-<AppContent class="app-content">
-  <main class="main-content">
-    <Canvas shadows size={{height: innerHeight, width: innerWidth}}
-  rendererParameters={{logarithmicDepthBuffer: true}}>
+      <T.PointLight color="white" intensity={.2} position={[0, 5, 0]} />
+      <T.PointLight color="blue" intensity={0.5} position={[5, 5, 0]} />
+      <T.PointLight color="yellow" intensity={0.5} position={[-5, -5, 0]} />
 
-  <T.PointLight color="white" intensity={.2} position={[0, 5, 0]} />
-  <T.PointLight color="blue" intensity={0.5} position={[5, 5, 0]} />
-  <T.PointLight color="yellow" intensity={0.5} position={[-5, -5, 0]} />
+      <T.PerspectiveCamera
+        makeDefault
+        position={[.6, .6, .6]} fov={25}>
+        <OrbitControls enableZoom={true} />
+      </T.PerspectiveCamera>
+  
+      <ThreeStage preset_name="soft" />
+      <Grid />
 
-  <T.PerspectiveCamera makeDefault let:ref={camera}
-    position={[.6, .6, .6]} fov={25}>
-    <OrbitControls enableZoom={true} />
-  </T.PerspectiveCamera>
-
-  <ThreeStage preset_name="soft" />
-  <Grid />
-
-  {#if $robot_urdf}
-    <UrdfThree />
-  {/if}
-</Canvas>
-  </main>
-</AppContent>
+      {#if urdf_viewer_state.robot}
+        <UrdfThree></UrdfThree>
+      {/if}
+    </Canvas>
+  </div>
+</main>
