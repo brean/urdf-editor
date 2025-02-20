9c5f166a8fbde2fc7a8bace289d829d5d49387ef
Andreas Bresser <self@andreasbresser.de>
2025-02-21 00:06:09 +0100

    use 2 spaces in MonacoEditor

--- src/lib/components/MonacoEditor.svelte
+++ src/lib/components/MonacoEditor.svelte
@@ -1,65 +1,64 @@
 <script lang="ts">
-	import loader from '@monaco-editor/loader';
-	import { onDestroy, onMount } from 'svelte';
-	import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+  import loader from '@monaco-editor/loader';
+  import { onDestroy, onMount } from 'svelte';
+  import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
-	let monaco: typeof Monaco;
-    let editor = $state();
-	let editorContainer: HTMLElement;
+  let monaco: typeof Monaco;
+  let editor = $state();
+  let editorContainer: HTMLElement;
 
-    interface Props {
-        text: string
+  interface Props {
+      text: string
+  }
+  let {
+      text = $bindable('')
+  }: Props = $props();
+
+  onMount(async () => {
+    // Remove the next two lines to load the monaco editor from a CDN
+    // see https://www.npmjs.com/package/@monaco-editor/loader#config
+    const monacoEditor = await import('monaco-editor');
+    loader.config({ 
+      monaco: monacoEditor.default,
+    });
+
+    monaco = await loader.init();
+    // Your monaco instance is ready, let's display some code!
+    editor = monaco.editor.create(
+      editorContainer,
+      {
+        value: text,
+        language: 'xml',
+        theme: 'vs-dark',
+        minimap: { enabled: false },
+        automaticLayout: true,
+        scrollBeyondLastLine: false
+      });
+  });
+
+  onDestroy(() => {
+    if (!monaco) {
+      return
     }
-    let {
-        text = $bindable('')
-    }: Props = $props();
+    monaco.editor.getModels().forEach(
+      (model) => model.dispose());
+  });
 
-	onMount(async () => {
-		// Remove the next two lines to load the monaco editor from a CDN
-		// see https://www.npmjs.com/package/@monaco-editor/loader#config
-		const monacoEditor = await import('monaco-editor');
-		loader.config({ 
-            monaco: monacoEditor.default,
-         });
-
-		monaco = await loader.init();
-		// Your monaco instance is ready, let's display some code!
-        editor = monaco.editor.create(
-            editorContainer,
-            {
-                value: text,
-                language: 'xml',
-                theme: 'vs-dark',
-                minimap: { enabled: false },
-                automaticLayout: true,
-                scrollBeyondLastLine: false
-            });
-	});
-
-	onDestroy(() => {
-        if (!monaco) {
-            return
-        }
-		monaco.editor.getModels().forEach((model) => model.dispose());
-	});
-
-    $effect(() => {
-        if (!editor) {
-            console.log('monaco not loaded')
-            return
-        }
-        console.log(text)
-        editor.setValue(text)
-		// monaco.editor.getModels().setValue(text);
-    })
+  $effect(() => {
+    if (!editor) {
+      console.log('monaco not loaded')
+      return
+    }
+    editor.setValue(text);
+  })
 </script>
 
 <div class="container" bind:this={editorContainer} ></div>
 
 
 <style>
-	.container {
-		width: 100%;
-		height: 100%;
-	}
+  .container {
+    width: 100%;
+    height: 100%;
+  }
 </style>
